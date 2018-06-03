17ff4ec7bf1b58914ddad08535c80c47f4069b99
huynhsamha <huynhsamha@gmail.com>
2018-06-03 19:50:49 +0700

    fix layout freecodecamp

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(express.static('public'));
+
 import routes from './server/routes';
 
 app.use('/', routes);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,27 @@
 import path from 'path';
 import express from 'express';
 import multer from 'multer';
+import fs from 'fs';
 
 const router = express.Router();
-const upload = multer({ dest: path.join(__dirname, '../tmp') });
+const upload = multer({
+  dest: path.join(__dirname, '../tmp')
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../views/home.html'));
 });
 
-router.post('/get-file-size', upload.single('file'), (req, res, next) => {
-  res.send({ size: req.file.size });
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+router.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  res.send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+  fs.unlink(req.file.path, (err) => {
+    if (err) console.log(err); else console.log('Remove a file!');
+  });
 });
 
 export default router;
